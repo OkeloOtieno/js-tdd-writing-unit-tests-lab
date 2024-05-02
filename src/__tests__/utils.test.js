1019a81045fc06e18b7211dd523cceb20c4ab784
Emmanuel <emmanuelokello294@gmail.com>
2024-05-02 11:08:05 +0300

    completes lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,19 @@
-// Your tests here
+import { isPalindrome } from "../utils"
+
+describe('isPalindrome', ()=>{
+    it('it should return true for "racecar"', ()=>{
+         expect(isPalindrome("racecar")).toBe(true)
+    })
+    it('it should return false for "car"', ()=>{
+         expect(isPalindrome("car")).toBe(false)
+    })
+    it('it should return true for "raceCar"', ()=>{
+        expect(isPalindrome("raceCar")).toBe(true)
+    })
+    it('it should return false for empty string', ()=>{
+        expect(isPalindrome("")).toBe(false)
+    })
+    it("it should throw an error for Numbers", ()=>{
+        expect(() => isPalindrome("racec1r")).toThrow('Contains Numbers');
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
-// Your code here
+export function isPalindrome(str) {
+    if(str.length === 0){
+        return false
+    }
+
+    const cleanedStr = str.toLowerCase().replace(/[\W_]/g, '');
+
+    if (/\d/.test(cleanedStr)) {
+        throw new Error('Contains Numbers');
+    }
+
+    
+    return cleanedStr === cleanedStr.split('').reverse().join('');
+}
